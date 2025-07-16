9fa0056cf639cb28844951c870fe967dcb2a0719
hatem elbadwy <mohamed.ahmed@8worx.com>
2025-07-16 10:59:02 +0300

    Update record package

--- lib/provider/sound_record_notifier.dart
+++ lib/provider/sound_record_notifier.dart
@@ -23,7 +23,7 @@ class SoundRecordNotifier extends ChangeNotifier {
   String initialStorePathRecord = "";
 
   /// recording mp3 sound Object
-  Record recordMp3 = Record();
+  AudioRecorder recordMp3 = AudioRecorder();
 
   /// recording mp3 sound to check if all permisiion passed
   bool _isAcceptedPermission = false;
@@ -226,7 +226,10 @@ class SoundRecordNotifier extends ChangeNotifier {
       buttonPressed = true;
       String recordFilePath = await getFilePath();
       _timer = Timer(const Duration(milliseconds: 900), () {
-        recordMp3.start(path: recordFilePath);
+        recordMp3.start(const RecordConfig(
+          encoder: AudioEncoder.amrNb,
+          sampleRate: 8000,
+        ),path: recordFilePath);
       });
       _mapCounterGenerater();
       notifyListeners();
